2fd2f32f7e942802407f21d4f83e522272ea4708
Oleg Potemkin <opotemkin@mail.ru>
2018-03-23 00:01:47 +0500

    Update MarkdownEditor.php

--- MarkdownEditor.php
+++ MarkdownEditor.php
@@ -527,7 +527,7 @@ EOT;
         // Move iframe at the end of the body
         $view->registerJs('jQuery("body").append(jQuery("#' . $this->_iframeId . '"));');
         // Initialize markdown editor after iframe is loaded
-        $js = 'jQuery(window).on('load', function(){initEditor(' . Json::encode($params) . ')});';
+        $js = 'jQuery(window).on("load", function(){initEditor(' . Json::encode($params) . ')});';
         $view->registerJs($js);
     }
 
